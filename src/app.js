8baa4ba850b6d0974bf2f0f44210918f7a03c962
陈赛 <chens@weipaitang.com>
2021-09-26 20:27:39 +0800

    feat: 修改适配方案

--- src/app.js
+++ src/app.js
@@ -3,10 +3,29 @@ import 'lib-flexible';
 
 // zarm 组件定制主题
 document.documentElement.style.setProperty('--theme-primary', '#1890ff');
-const isMobileMode = /Android|webOS|iPhone|iPod|BlackBerry/i.test(
-  window.navigator.userAgent,
-);
-if (!isMobileMode) {
-  // iPhone5 1rem=32px
-  document.documentElement.style.fontSize = '32px';
+
+// 适配pc/mobile客户端
+function adjustMode() {
+  const isMobileMode = /Android|webOS|iPhone|BlackBerry/i.test(
+    window.navigator.userAgent,
+  );
+  const isPadMode = /iPad|iPod/i.test(window.navigator.userAgent);
+  if (isMobileMode) {
+    // 移动端模式
+    return true;
+  } else if (isPadMode) {
+    // pad模式，淘宝弹性布局方案lib-flexible不兼容ipad和ipad pro的解决方法
+    let head, viewport;
+    /(iPad|iPod)/i.test(navigator.userAgent) &&
+      ((head = document.getElementsByTagName('head')),
+      (viewport = document.createElement('meta')),
+      (viewport.name = 'viewport'),
+      (viewport.content =
+        'target-densitydpi=device-dpi, width=480px, user-scalable=no'),
+      head.length > 0 && head[head.length - 1].appendChild(viewport));
+  } else {
+    // pc模式，按照iPhone5尺寸适配
+    document.documentElement.style.fontSize = '32px';
+  }
 }
+adjustMode();
